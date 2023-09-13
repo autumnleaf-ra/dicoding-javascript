b15934819c73eabeb872b1d5856f6bf30a9d8fa0
Muhammad <autumnleafra@gmail.com>
2023-09-13 13:19:59 +0700

    dicoding javascript

---
+++ restaurant.js
@@ -0,0 +1,11 @@
+const restaurant = 
+{
+"name" : "Bakso Mang Dicoding",
+"city" : "Bandung",
+"favorite drink" : "Es teh",
+"favorite food" : "Bakso",
+isVegan : false
+}
+
+var name = restaurant.name;
+var favoriteDrink = restaurant["favorite drink"];
